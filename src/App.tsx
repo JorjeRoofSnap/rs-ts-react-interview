7dd9d4dd214766dcdaebab0bdd0af7c1baedb8c0
Jorje Nava <jorje@roofsnap.com>
2022-10-07 15:33:27 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import NumberLister from './NumberLister';
+import MyProfile from './MyProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>Hello World,
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Number Lister</Link>
+            </li>
+            <li>
+              <Link to="/myprofile">My Profile</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<NumberLister />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
